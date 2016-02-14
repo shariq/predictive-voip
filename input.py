d3b51bbbbf9ec086fdce7c33c66892e662c9439d
Shariq Hashme <shariqhashme@gmail.com>
2016-02-14 14:13:09 -0500

    ugh python not fast enough

--- input.py
+++ input.py
@@ -5,13 +5,45 @@ this python script must send udp packets from the mic with audio data
 from pysoundcard import InputStream, continue_flag
 import time
 from Queue import Queue
+import pdb
+import traceback
+import threading
+
+import socket
+port = 7485
+host = 'localhost'
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 udp_queue = Queue()
 
+def get_timestamp_str(timestamp):
+    timestamp_str = ''
+    for i in range(4):
+        timestamp_str += chr(timestamp % 256)
+        timestamp = timestamp / 256
+    return timestamp_str
+
+def forever_send_udp_queue():
+    timestamp = 0
+    while True:
+        try:
+            data = udp_queue.get()
+            timestamp += 1
+            #timestamp_str = get_timestamp_str(timestamp)
+            #msg = timestamp_str + data.tostring()
+            #sock.sendto(msg, (host, port))
+            #sock.sendto(data.tostring(), (host,port))
+        except:
+            traceback.print_exc()
+
+#threading.Thread(target = forever_send_udp_queue).start()
+
 def callback(in_data, time_info, status):
-    udp_queue.put(in_data)
+    #udp_queue.put(in_data)
+    sock.sendto((in_data).tostring(), (host, port))
     return continue_flag
 
-record_stream = InputStream(samplerate=16000, blocksize=16, dtype='int8', channels=1)
+record_stream = InputStream(samplerate=16000, blocksize=1024, dtype='int32', channels=1, callback=callback)
 record_stream.start()
 
 # now write something which takes stuff from the udp queue and sends it over
--- output.py
+++ output.py
@@ -7,22 +7,51 @@ import time
 from Queue import Queue
 import threading
 import traceback
+import numpy
 
-playback_stream = OutputStream(samplerate=16000, blocksize=16, dtype='int8', channels=1)
+import socket
+port = 7485
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', port))
+
+last_timestamp = 0
+
+def get_timestamp(data):
+    timestamp = 0
+    for i in range(4):
+        timestamp += ord(data[i]) * (256**i)
+    return timestamp
+
+def forever_receive_from_udp():
+    while True:
+        try:
+            data, addr = sock.recvfrom(1000)
+            # do some stuff with data maybe before sticking it into playback_queue
+            #timestamp = get_timestamp(data)
+            #playback_queue.put(numpy.fromstring(data[4:]))
+            playback_stream.write(numpy.fromstring(data))
+        except:
+            traceback.print_exc()
+
+t = threading.Thread(target = forever_receive_from_udp)
+t.setDaemon(True)
+t.start()
+
+playback_stream = OutputStream(samplerate=16000, blocksize=1024, dtype='int32', channels=1)
 playback_stream.start()
-write_queue = Queue()
+playback_queue = Queue()
 
 def forever_write_to_output():
     while True:
         try:
-            playback_stream.write(write_queue.get())
+            playback_stream.write(playback_queue.get())
         except:
             traceback.print_exc()
 
-threading.Thread(target = forever_write_to_output).start()
+#threading.Thread(target = forever_write_to_output).start()
 
 # now write something which receives udp packets and sticks
-# them into the write queue
+# them into the playback queue
 
 while True:
     try:
